0711e9cc67461984eee241ba8a2399e7db92ce8d
Farid Lopez <faridlopez5@gmail.com>
2025-05-23 15:29:04 -0700

    Initial Commit

---
+++ WEDERReminderTests/WEDERReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  WEDERReminderTests.swift
+//  WEDERReminderTests
+//
+//  Created by Farid Lopez on 5/23/25.
+//
+
+import Testing
+@testable import WEDERReminder
+
+struct WEDERReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
